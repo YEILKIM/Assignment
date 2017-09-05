50f0dbc2f7f6995c1689d747c08668bf34560bb4
YEILKIM <31638901+YEILKIM@users.noreply.github.com>
2017-09-05 11:41:47 +0900

    Add files via upload

---
+++ FirstAssignment.java
@@ -0,0 +1,12 @@
+
+public class FirstAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sudent ID: 2016301012");
+		System.out.println("Name: ±è¿¹ÀÏ");
+		System.out.println("M.P num: 010 4190 8545");
+		System.out.println("github.com/YEILKIM");
+	}
+
+}
