fb28c61f0ba94ee0f202464e3de6c88165772b77
YEILKIM <31638901+YEILKIM@users.noreply.github.com>
2017-09-13 20:41:10 +0900

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Sports{
+	
+	String getName(){
+		return "Generic Sports";
+	}
+	void getNumberOfTeamMembers(){
+		System.out.println("Each team has n plyers in " + getName());
+	}
+}
+
+class Soccer extends Sports{
+	@Override
+	String getName(){
+		return "Soccer Class";
+	}
+	void getNumberOfTeamMembers(){
+		System.out.println("Each team has 11 players in "+getName());
+	}
+}
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		
+		Sports sport = new Sports();
+		Soccer soccer = new Soccer();
+		System.out.println(sport.getName());
+		sport.getNumberOfTeamMembers();
+		System.out.println(soccer.getName());
+		soccer.getNumberOfTeamMembers();
+
+	}
+
+}
